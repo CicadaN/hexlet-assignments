5f697e942b61cb70a03122aee307e6821df8d48e
@hexlet/cli <support@hexlet.io>
2024-02-12 17:11:22 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public static boolean scrabble(String abc, String word) {
+    word = word.toLowerCase();
+    for (int i = 0; i < word.length(); i++) {
+        char ch = word.charAt(i);
+        if (abc.indexOf(ch) == -1) {
+            return false;
+        }
+        abc = abc.substring(0, abc.indexOf(ch)) + abc.substring(abc.indexOf(ch) + 1)
+    }
+    return true;
+}
+//END
