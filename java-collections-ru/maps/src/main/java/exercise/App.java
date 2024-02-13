6c89e42798a0d9b753e0b54823bfa7e7da1d4382
@hexlet/cli <support@hexlet.io>
2024-02-13 19:47:57 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,28 +6,36 @@ import java.util.Map;
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordCount = new HashMap<>();
-
         String[] words = sentence.split(" ");
 
         for (String word : words) {
-            if (!wordCount.containsKey(word)) {
-                wordCount.put(word, 1);
-            } else {
-                int count = wordCount.get(word);
-                wordCount.put(word, count + 1);
+            if (!word.isEmpty()) {
+                if (!wordCount.containsKey(word)) {
+                    wordCount.put(word, 1);
+                } else {
+                    int count = wordCount.get(word);
+                    wordCount.put(word, count + 1);
+                }
             }
         }
+
         return wordCount;
     }
 
     public static String toString(Map<String, Integer> wordCount) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{\n");
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-            sb.append("  " + entry.getKey() + ": " + entry.getValue() + "\n");
+            result.append("  ")
+                    .append(entry.getKey())
+                    .append(": ")
+                    .append(entry.getValue())
+                    .append("\n");
         }
-        sb.append("}");
-        return sb.toString();
+
+        result.append("}");
+
+        return result.toString();
     }
 }
 //END
