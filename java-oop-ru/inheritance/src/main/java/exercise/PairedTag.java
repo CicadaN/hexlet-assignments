6f553d866d8f0c2c6593f622acabae14d44ba1bc
@hexlet/cli <support@hexlet.io>
2024-07-11 23:07:07 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -6,8 +6,25 @@ import java.util.stream.Collectors;
 
 // BEGIN
 public class PairedTag extends Tag{
-    private
+    private String tagContent;
+    private List<Tag> children;
 
+    public PairedTag(String tagName, Map<String, String> attributes, String tagContent, List<Tag> children) {
+        super(tagName, attributes);
+        this.tagContent = tagContent;
+        this.children = children;
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("<%s %s>", tagName, getAttributesString()));
+        for (Tag child : children) {
+            sb.append(child.toString());
+        }
+        sb.append(tagContent);
+        sb.append(String.format("</%s>", tagName));
+        return sb.toString();
+    }
 }
 // END
